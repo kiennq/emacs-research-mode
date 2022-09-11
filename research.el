41cd7361f17ec9d24e65943a59672cdd2f7e9ccd
Kien Nguyen <kien.n.quang@gmail.com>
2022-09-11 21:25:03 +0900

    fix: ci for emacs 26

--- research.el
+++ research.el
@@ -169,7 +169,9 @@ ERASE? will clear the log buffer, and POPUP? wil switch to it."
 (cl-defmethod research--re-auth-p (host (_auth (eql 'cookie)) (_forge (eql 'cs-github)))
   "Check if can re-authenticate for HOST of GitHub Codesearch."
   (when-let* ((has-domain? (string-match
-                            (rx (*? anything) (* ?.) (group  (+? (not ?.)) ?. (+? (not ?.))) eol)
+                            (rx (*? anything) (* ?.)
+                                (group  (+? (not (any ?.))) ?. (+? (not (any ?.))))
+                                eol)
                             host))
               (domain (match-string 1 host)))
     (url-cookie-delete-cookies domain)
